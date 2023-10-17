9467d188a993e98f5a9e6b86cb99982a35791bb4
DDUDII <qortn121@naver.com>
2023-10-18 01:36:11 +0900

    feat: 상품 data 파일 생성
    feat: 상품 data 파일 생성

---
+++ src/data.js
@@ -0,0 +1,35 @@
+let data = [
+  {
+    id : 0,
+    title : "Black outer",
+    content : "Born in France",
+    price : 210000,
+    img: "/img/black-outer.jpg"
+  },
+
+  {
+    id : 1,
+    title : "Leather pants",
+    content : "Born in Seoul",
+    price : 110000,
+    img: "/img/leather-pants.jpg"
+  },
+
+  {
+    id : 2,
+    title : "Joker pants",
+    content : "Born in the States",
+    price : 130000,
+    img: "/img/joker-pants.jpg"
+  },
+
+  {
+    id : 3,
+    title : "White t-shirts",
+    content : "Born in the States",
+    price : 80000,
+    img: "/img/t-shirts.jpg"
+  }
+] 
+
+export default data;
