f01cccf83c9c61588d8eb6a5a300b82e694158ca
DDUDII <qortn121@naver.com>
2023-11-03 00:07:13 +0900

    feat: details 하단 tap 추가
    feat: details 하단 tap 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Row, Nav } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -6,6 +7,12 @@ function Detail({ clothes }) {
   let { id } = useParams();
   const itemId = parseInt(id, 10); //URL에서 얻은 매개변수는 URL 경로에서 가져온 것이므로 일반적으로 문자열
   const item = clothes.find((item) => item.id === itemId);
+  const [tap, setTap] = useState(0);
+
+  const tapHandler = (eventKey) => {
+    setTap(Number(eventKey));
+  };
+
   return (
     <Container>
       <Row>
@@ -24,8 +31,38 @@ function Detail({ clothes }) {
           <button className="btn btn-danger">주문하기</button>
         </Col>
       </Row>
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="0" onClick={() => tapHandler(0)}>
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="1" onClick={() => tapHandler(1)}>
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="2" onClick={() => tapHandler(2)}>
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tap={tap} />
     </Container>
   );
+}
+
+function TabContent({ tap }) {
+  if (tap === 0) {
+    return <div>내용0</div>;
+  }
+  if (tap === 1) {
+    return <div>내용1</div>;
+  }
+  if (tap === 2) {
+    return <div>내용2</div>;
+  }
 }
 
 export default Detail;
