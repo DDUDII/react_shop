13b47e6943c784a0b74f582e132a69c9b9749702
DDUDII <qortn121@naver.com>
2023-11-10 01:26:00 +0900

    feat: 수량 버튼 클릭시 수량 더하기 빼기 기능 추가
    feat: 수량 버튼 클릭시 수량 더하기 빼기 기능 추가

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,41 +1,52 @@
 import { Table } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { changeName } from "../store/userSlice";
+import { addCount, minusCount } from "../store/cartSlice";
 
-function Cart() {
-  const state = useSelector((state) => state);
+function Cart({ clothes }) {
+  const cartState = useSelector((state) => state.cart);
+  const userState = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   return (
     <div>
-      <h6>{state.user.name}</h6>
-      <button
-        onClick={() => {
-          dispatch(changeName());
-        }}
-      >
-        변경
-      </button>
       <Table>
         <thead>
           <tr>
-            <th>순번</th>
+            <th>상품코드</th>
             <th>상품명</th>
             <th>수량</th>
             <th>변경하기</th>
           </tr>
         </thead>
         <tbody>
-          {state.cart.map((value, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{state.cart[i].name}</td>
-              <td>{state.cart[i].count}</td>
-              <td>
-                <button>변경</button>
-              </td>
-            </tr>
-          ))}
+          {cartState.map((value, i) => {
+            const { name, count, id } = cartState[i];
+
+            return (
+              <tr key={i}>
+                <td>{id + 1}</td>
+                <td>{name}</td>
+                <td>{count}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(addCount(id));
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => {
+                      dispatch(minusCount(id));
+                    }}
+                  >
+                    -
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
