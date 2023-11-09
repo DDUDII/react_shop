97e1671a7a91b2ac691f323a2db24cc94befa0f2
DDUDII <qortn121@naver.com>
2023-11-10 01:27:33 +0900

    feat: count 변경 state 추가
    feat: count 변경 state 추가

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -6,6 +6,23 @@ const cart = createSlice({
     { id: 0, name: "Black outer", count: 1 },
     { id: 1, name: "Leather pants", count: 2 },
   ],
+  reducers: {
+    addCount(state, action) {
+      const cartId = state.findIndex((state) => {
+        return state.id === action.payload;
+      });
+      state[cartId].count++;
+    },
+    minusCount(state, action) {
+      const cartId = state.findIndex((state) => {
+        return state.id === action.payload;
+      });
+      state[cartId].count--;
+    },
+    addItem(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
-
+export let { addCount, minusCount, addItem } = cart.actions;
 export default cart;
