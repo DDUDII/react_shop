563edee7f1adf256ac237e36fd0cb1655ab503d6
DDUDII <qortn121@naver.com>
2023-11-12 04:19:52 +0900

    feat: 최근 본 상품 기능, localStorage 데이터 추가
    feat: 최근 본 상품 기능, localStorage 데이터 추가

--- src/conponents/Watched.js
+++ src/conponents/Watched.js
@@ -1,11 +1,35 @@
 import { Col } from "react-bootstrap";
 import s from "./watched.module.css";
+import { getWatched, removeWatched } from "../assets/constant";
+import WATCHED_KEY from "../assets/constant";
 
-function Watched() {
+function Watched({ clothes }) {
+  const watchedItems = getWatched();
+  const removeItems = () => {
+    removeWatched();
+  };
   return (
     <Col sm>
-      <h4 className={s.watchedTitle}>최근 본 상품</h4>
-      <div className={s.watchedContent}>내용</div>
+      <div className={s.container}>
+        <h4 className={s.watchedTitle}>최근 본 상품</h4>
+        <button className={s.removeBtn} onClick={removeItems}>
+          비우기
+        </button>
+        <div className={s.watchedContent}>
+          {watchedItems.map((itemId) => {
+            const item = clothes.find((item) => item.id === itemId);
+            const { title, img } = item;
+            return (
+              <img
+                className={s.watchedImg}
+                key={itemId}
+                src={process.env.PUBLIC_URL + img}
+                alt={title}
+              />
+            );
+          })}
+        </div>
+      </div>
     </Col>
   );
 }
